551609488701ad2b1e6261782fcac7214e7defef
Jason Franklin <40970853+Jason-Franklin@users.noreply.github.com>
2020-07-23 09:48:24 -0500

    Update RunScheduledTasksRemotely.ps1

--- RunScheduledTasksRemotely.ps1
+++ RunScheduledTasksRemotely.ps1
@@ -7,20 +7,28 @@
 
 #Get credentials for service account login
 #Change "ServiceAccountName" with desired service account
+
 $cred = Get-Credential ServiceAccountName
+
 #Path where script and .txt file with server list resides. Change this to the path of the location you desire.
+
 $path = 'C:\Users\YourDirectory'
+
 #Get content of server list file
+
 $servers = get-content $path\ScheduledTaskHostList.txt
 
 #Show progress of tasks
+
 Write-progress -Activity "Running tasks." -currentoperation "logging into hosts...."
 
 Foreach ($hostName in $servers) {
 
 $sess = New-PSSession -Credential $cred -ComputerName $hostname
+
 #Run scheduled task and write output to powershell window when successful
 #Change "Scheduled Task Name" to your desired scheduled task
+
 Invoke-Command -session $sess -scriptblock { 
     Start-ScheduledTask -TaskName "Scheduled Task Name"| get-scheduledtaskinfo
     Write-Host "$Using:hostName > Successful."
